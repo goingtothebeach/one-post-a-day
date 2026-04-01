3cc474cb1c93fe0530dfa97338449c99308a66d4
guoyixuan03 <guoyxssy@gmail.com>
2026-04-01 12:06:08 +0800

    Fix: serve MaterialIcons.ttf from root path, fix vercel.json rewrites

--- scripts/inject-fonts.js
+++ scripts/inject-fonts.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexPath = path.join(__dirname, '..', 'dist', 'index.html');
+const distDir = path.join(__dirname, '..', 'dist');
+const indexPath = path.join(distDir, 'index.html');
+
+const ttfSrc = path.join(distDir, 'assets/node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.4e85bc9ebe07e0340c9c4fc2f6c38908.ttf');
+const ttfDest = path.join(distDir, 'MaterialIcons.ttf');
+if (fs.existsSync(ttfSrc)) {
+  fs.copyFileSync(ttfSrc, ttfDest);
+}
+
 let html = fs.readFileSync(indexPath, 'utf8');
 
-const fontLink = '<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />';
+html = html.replace(/@font-face\{font-family:"MaterialIcons";src:url\("[^"]*"\)[^}]*\}/g, '');
+html = html.replace(/<link rel="preload" href="[^"]*MaterialIcons[^"]*" as="font"[^>]*\/>/g, '');
+html = html.replace(/<link rel="stylesheet" href="https:\/\/fonts\.googleapis\.com\/icon\?family=Material\+Icons" \/>/g, '');
+html = html.replace(/<style>@font-face\{font-family:"MaterialIcons"[^<]*<\/style>/g, '');
 
-if (!html.includes('Material+Icons')) {
-  html = html.replace('</head>', `${fontLink}</head>`);
-  fs.writeFileSync(indexPath, html);
-  console.log('Injected Material Icons font link');
-} else {
-  console.log('Material Icons already present');
-}
+const inject = '<style>@font-face{font-family:"MaterialIcons";src:url("/MaterialIcons.ttf");font-display:block}</style>';
+
+html = html.replace('</head>', `${inject}</head>`);
+fs.writeFileSync(indexPath, html);
+console.log('Injected MaterialIcons font at /MaterialIcons.ttf');
