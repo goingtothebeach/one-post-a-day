24a20fef1c286954367f53c8335752de694787c6
guoyixuan03 <guoyxssy@gmail.com>
2026-04-01 14:58:14 +0800

    Fix: serve per-route HTML files + disable SSR hydration to fix tab active state

--- scripts/inject-fonts.js
+++ scripts/inject-fonts.js
@@ -1,24 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const { globSync } = require('fs');
 
 const distDir = path.join(__dirname, '..', 'dist');
-const indexPath = path.join(distDir, 'index.html');
 
-const ttfSrc = path.join(distDir, 'assets/node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.4e85bc9ebe07e0340c9c4fc2f6c38908.ttf');
-const ttfDest = path.join(distDir, 'MaterialIcons.ttf');
-if (fs.existsSync(ttfSrc)) {
-  fs.copyFileSync(ttfSrc, ttfDest);
+function getAllHtmlFiles(dir) {
+  const results = [];
+  const items = fs.readdirSync(dir);
+  for (const item of items) {
+    const full = path.join(dir, item);
+    const stat = fs.statSync(full);
+    if (stat.isDirectory()) {
+      results.push(...getAllHtmlFiles(full));
+    } else if (item.endsWith('.html')) {
+      results.push(full);
+    }
+  }
+  return results;
 }
 
-let html = fs.readFileSync(indexPath, 'utf8');
+const htmlFiles = getAllHtmlFiles(distDir);
 
-html = html.replace(/@font-face\{font-family:"MaterialIcons";src:url\("[^"]*"\)[^}]*\}/g, '');
-html = html.replace(/<link rel="preload" href="[^"]*MaterialIcons[^"]*" as="font"[^>]*\/>/g, '');
-html = html.replace(/<link rel="stylesheet" href="https:\/\/fonts\.googleapis\.com\/icon\?family=Material\+Icons" \/>/g, '');
-html = html.replace(/<style>@font-face\{font-family:"MaterialIcons"[^<]*<\/style>/g, '');
+for (const filePath of htmlFiles) {
+  let html = fs.readFileSync(filePath, 'utf8');
 
-const inject = '<style>@font-face{font-family:"MaterialIcons";src:url("/MaterialIcons.ttf");font-display:block}</style>';
+  html = html.replace(
+    'globalThis.__EXPO_ROUTER_HYDRATE__=true;',
+    'globalThis.__EXPO_ROUTER_HYDRATE__=false;'
+  );
 
-html = html.replace('</head>', `${inject}</head>`);
-fs.writeFileSync(indexPath, html);
-console.log('Injected MaterialIcons font at /MaterialIcons.ttf');
+  fs.writeFileSync(filePath, html);
+}
+
+console.log(`Patched ${htmlFiles.length} HTML files: disabled SSR hydration`);
